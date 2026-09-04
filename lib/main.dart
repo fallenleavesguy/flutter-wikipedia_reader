5e5267335b7c958663f93dafa068fc8ca49d472c
donghs <no@email.com>
2026-09-04 13:59:07 +0800

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,48 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = ArticleViewModel(ArticleModel());
+    return const MaterialApp(home: ArticleView());
+  }
+}
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Wikipedia Flutter')),
-        body: const Center(child: Text('Loading...')),
+class ArticleView extends StatefulWidget {
+  const ArticleView({super.key});
+
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState  extends State<ArticleView> {
+  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  void initState() {
+    super.initState();
+
+    viewModel.fetchArticle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Wikipedia Flutter')),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, asyncSnapshot) {
+          return switch((
+            viewModel.isLoading,
+            viewModel.summary,
+            viewModel.error,
+          )) {
+            (true, _, _) => const CircularProgressIndicator(),
+            (_, _, final Exception e) => Text('Error: $e'),
+            (_, final summary?, _) => ArticlePage(
+              summary: summary,
+              nextArticleCallback: viewModel.fetchArticle,
+            ),
+            _ => const Text("Something went wrong"),
+          };
+        },
       ),
     );
   }
@@ -34,7 +70,7 @@ class ArticleModel {
     );
     final response = await get(uri);
 
-     if (response.statusCode != 200) {
+    if (response.statusCode != 200) {
       throw const HttpException('Failed to update resource');
     }
 
@@ -72,3 +108,62 @@ class ArticleViewModel extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(onPressed: nextArticleCallback, child: const Text('Next Random Article'))
+        ]
+      )
+    );
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({
+    super.key,
+    required this.summary,
+  });
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          Text(summary.extract),
+        ],
+      ),
+    );
+  }
+}
+
